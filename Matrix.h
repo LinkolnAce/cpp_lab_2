c8697181dff3cbee25a66ade09563470231ab168
LinkolnAce <91007494+LinkolnAce@users.noreply.github.com>
2021-10-13 14:17:27 +0300

    Source.cpp had main func

---
+++ Matrix.h
@@ -0,0 +1,25 @@
+#pragma once
+class Matrix
+{
+public:
+	Matrix(const int rows_, const int cols_);
+	~Matrix();
+
+	void operator++();
+	void operator++(int);
+	void operator--();
+	void operator--(int);
+
+	int at(int i, int j);
+	void setAt(int i, int j, int val);
+
+	int getRows();
+	int getCols();
+
+	void printFormated();
+private:
+	int rows_;
+	int cols_;
+	int** matrix;
+};
+
